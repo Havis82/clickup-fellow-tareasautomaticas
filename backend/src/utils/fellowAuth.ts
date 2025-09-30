e5f5c0a623ce8a0642ba4f57cdde7040f75696f3
Havis82 <jacor82@hotmail.com>
2025-09-30 13:17:10 +0200

    Update fellowAuth.ts con código para manejar autenticación con Fellow

--- backend/src/utils/fellowAuth.ts
+++ backend/src/utils/fellowAuth.ts
@@ -1,1 +1,9 @@
-// TODO: implement
+import axios from "axios";
+
+axios.interceptors.request.use((config) => {
+  config.headers = {
+    ...config.headers,
+    "X-API-KEY": FELLOW_API_KEY
+  };
+  return config;
+});// TODO: implement
