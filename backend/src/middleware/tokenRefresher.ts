950a656629f9d64ebbdd7101f0fe8bf59a74e343
Havis82 <jacor82@hotmail.com>
2025-09-30 13:05:49 +0200

    Update tokenRefresher.ts

--- backend/src/middleware/tokenRefresher.ts
+++ backend/src/middleware/tokenRefresher.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { refreshToken } from '../services/tokenRefresher';
+import { refreshToken } from '../middleware/tokenRefresher';
 
 export async function tokenRefresherMiddleware(req: Request, res: Response, next: NextFunction) {
   if (req.isAuthenticated() && req.user && 'refreshToken' in req.user) {
