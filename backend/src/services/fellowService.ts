e3bba4061f8a069c9f931df5fb7ee7e94a3a89ce
Havis82 <jacor82@hotmail.com>
2025-09-30 13:20:19 +0200

    Update fellowService.ts con c´ódigo para acceder a la API de Fellow

--- backend/src/services/fellowService.ts
+++ backend/src/services/fellowService.ts
@@ -1,1 +1,10 @@
-// TODO: implement
+import { getFellowAuthHeaders } from "../utils/fellowAuth";
+import axios from "axios";
+
+export async function fetchMeetingTranscription(meetingId: string) {
+  const url = `${process.env.FELLOW_API_BASE_URL}/meetings/${meetingId}/transcription`;
+  const headers = getFellowAuthHeaders();
+
+  const response = await axios.get(url, { headers });
+  return response.data;
+}
